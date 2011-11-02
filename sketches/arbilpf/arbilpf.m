a4edc238e378fbcd99e6ee74c6f5b6808ede8efa
jecs <jecs@lambdalabs.com>
2011-11-02 02:29:04 -0400

    edited arbilpf.m so that it accepts passband and stopband ripple as arguments; arbilpf_testing.m contains an example

--- sketches/arbilpf/arbilpf.m
+++ sketches/arbilpf/arbilpf.m
@@ -1,10 +1,12 @@
-function [Y] = arbilpf(X, M, N, dW)
+function [Y] = arbilpf(X, M, r, w, K)
 % Y = abrilpf(X,M)
-% X  - WxL or WxLx3 matrix containing the image data (should be of double precision)
-% N  - length of 1D LPF filter that is transformed to two dimensions (good ranges: 30-50)
-% dW - width of transition region of the LPF, in radians (increase for less ripple; dW = 0.05 is good)
-% M  - downsampling coefficient
-% Y  - lowpassed version of X with pi/M as the radial cutoff frequency
+% X - WxL or WxLx3 matrix containing the image data (should be of integer precision!)
+% M - downsampling coefficient
+% r - maximum passband ripple (good values: r <= 0.05)
+% w - width of transition region as a fraction of the passband of the LPF,
+%     ie, width of transition = w*(1/M) (w = 0.1 is good)
+% K - ratio of stopband ripple to passband ripple (the lower it is, the larger the filter)
+% Y - lowpassed version of X with pi/M as the radial cutoff frequency
 
 Y = zeros(size(X));
 
@@ -15,11 +17,27 @@ Y = zeros(size(X));
 % h2d = ftrans2(h1d)
 % freqz2(h2d) - plots the 2d frequency response of h2d
 
-h1d = firpm(N, [0, (1/M-dW), (1/M+dW), 1], [1, 1, 0, 0]);
-h2d = ftrans2(h1d);
-% freqz2(h2d)
-for i=1:size(Y, 3)
-	Y(:,:,i) = conv2(X(:,:,i), h2d, 'same');
+dW = w/M;
+N = 10; % starting length - 1
+err = r+0.1; % magnitude of the ripple
+while (err > r) % find lowest order filter that meets ripple specification
+	N = N+2; % N must be even for ftrans2
+	[h1d, err] = firpm(N, [0, (1/M-dW/2), (1/M+dW/2), 1], [1, 1, 0, 0], [K, 1]);
+	err = err/K;
 end
+h2d = ftrans2(h1d);
+
+% debugging statements
+filter_order = sprintf('1D filter length: %d; 2D filter size: %d', N+1, (size(h2d,1)*size(h2d,2))); disp(filter_order);
+% H1d = freqz(h1d);
+% figure; plot(linspace(-1, 1, size(H1d, 1)), abs(H1d));
+% freqz2(h2d)
+
+% for i=1:size(Y, 3)
+%	Y(:,:,i) = conv2(X(:,:,i), h2d, 'same');
+%end
+
+% using imfilter to speed up computation
+Y = imfilter(X, h2d, 'symmetric', 'same', 'conv');
 
 end
--- sketches/arbilpf/arbilpf_testing.m
+++ sketches/arbilpf/arbilpf_testing.m
@@ -1,17 +1,19 @@
 % simply testing arbilpf
 
 img_orig = imread('../arbiskew/IMG_5046.jpg', 'jpg');
-img_orig = im2double(img_orig);
-% size(img_orig)
-% imshow(img_orig);
-% M = 3
-img_lpf = arbilpf(img_orig, 3, 30, 0.05);
+img_orig_d = im2double(img_orig);
+M = 4; % downsampling factor
+P = 0.02; % maximum passband ripple
+K = 5; % stopband ripple = K*passband ripple
+T = 0.2; % transition width = T/M*pi (in rads)
+img_lpf = arbilpf(img_orig, M, P, T, K);
+img_lpf_d = im2double(img_lpf);
 figure;
 subplot(1,2,1); imshow(img_orig); title('Original Image');
 subplot(1,2,2); imshow(img_lpf); title('Low Passed Image');
 % plot fft of original image
-fft_orig = fftshift(fft2(rgb2gray(img_orig)));
-fft_lpf = fftshift(fft2(rgb2gray(img_lpf))); 
+fft_orig = fftshift(fft2(rgb2gray(img_orig_d)));
+fft_lpf = fftshift(fft2(rgb2gray(img_lpf_d))); 
 figure;
 subplot(1,2,1); imshow(log(abs(fft_orig(:,:,1))), [-1, 5]); colormap(jet); colorbar; title('Log Magnitude of FFT of Grayscaled Original');
 subplot(1,2,2); imshow(log(abs(fft_lpf(:,:,1))), [-1, 5]); colormap(jet); colorbar; title('Log Magnitude of FFT of Grayscaled LPFd Image');
