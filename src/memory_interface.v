3f630144ac9072387956b443af29451542a1f345
jecs <jecs@lambdalabs.com>
2011-11-17 01:58:21 -0500

    fixed a few bugs; finalized addressing logic; about to start handling interfacing with mem

--- src/memory_interface.v
+++ src/memory_interface.v
@@ -39,26 +39,30 @@ module memory_interface
 	reg disp_mem_block;
 
 	// LOCATIONS OF IMAGES IN EACH BLOCK
+	// making it a 2bit number to allow for the possibility
+	// of loading an image from flash and storing a third image
+	// for processing
 	reg [1:0] capt_mem_loc;
 	reg [1:0] proc_mem_loc;
 	reg [1:0] nexd_mem_loc;	
 	reg [1:0] disp_mem_loc;
 
 	// ADDRESSES
-	reg  [`LOG_ADDR:0] ntsc_addr;
-	reg  [`LOG_ADDR:0] vga_addr;
+	reg [`LOG_ADDR:0] ntsc_addr;
+	reg [`LOG_ADDR:0] vga_addr;
 	reg [`LOG_ADDR:0] lpf_addr;
 	reg [`LOG_ADDR:0] pt_addr;
 
 	// NEXT LOCS AND BLOCKS
-	reg next_blocks;
-	reg next_locs;
+	reg [3:0] next_blocks;
+	reg [7:0] next_locs;
 
 	always @(*) begin
 		// shifting
 		if (reset) begin
-			next_blocks = {0,0,0,0};
-			next_locs = {2'b00, 2'b00, 2'b00, 2'b00};
+			// choose starting condition such that capt and disp never overlap
+			next_blocks = {0,0,1,1};
+			next_locs = {2'b00, 2'b01, 2'b00, 2'b01};
 		end
 		else if (flame_flag) begin
 			next_blocks = {proc_mem_block, disp_mem_block, capt_mem_block, nexd_mem_block};
@@ -80,10 +84,16 @@ module memory_interface
 		{capt_mem_loc, proc_mem_loc, nexd_mem_loc, disp_mem_loc} <= next_locs;
 
 		// set addresses of NTSC and VGA / update if pixels have been read or written
-		ntsc_addr <= ntsc_addr + ((done_ntsc) ? 1 : 0);
-		vga_addr <= vga_addr + ((done_vga) ? 1 : 0);
+		ntsc_addr <= (reset) ? 0 : (ntsc_addr + done_ntsc);
+		vga_addr <= (reset) ? 0 : (vga_addr + done_vga);
+
+		// base pixel fed to mem on flag signal
 	end
 endmodule
+
+// TODO:
+// - add logic for all of the done flags
+// - interface with memory
 
 /* FOR REFERENCE
 	// SRAMs
