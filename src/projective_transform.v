b7e23a2eaa5222881f053610959de6af7db8e306
Logan Williams <loganw@mit.edu>
2011-11-21 15:01:45 -0500

    finished rough draft of projective_transform.v

--- src/projective_transform.v
+++ src/projective_transform.v
@@ -32,10 +32,23 @@ module projective_transform(
    reg [19:0] i_c_x;
    reg [18:0] i_c_y;
 
+   // iterator incrementors
+   reg [19:0] delta_a_x;
+   reg [19:0] delta_a_y;
+   reg [19:0] delta_b_x;
+   reg [19:0] delta_b_y;
+   reg [19:0] delta_c_x;
+   reg [19:0] delta_c_y;
+   reg [19:0] delta_c_x_next;
+   reg [19:0] delta_c_y_next;
+   
+
    // registers to store calculations of distance (fractional part does not matter)
    reg [9:0]  d_ad;
    reg [9:0]  d_bc;
    reg [9:0]  d_iterators;
+   reg [9:0]  d_next_iterators;
+   
 
    // temporary registers for calculating distances
    reg [19:0] temp1a;
@@ -61,11 +74,43 @@ module projective_transform(
    wire        sqrt_done_a;
    wire        sqrt_done_c;
    wire        sqrt_done_b;
-   
+
+   // wires/registers for diving
+   wire        rfd_a;
+   wire        rfd_b;
+   wire        rfd_c;
+   wire        rfd_d;
+   wire        rfd_e;
+   wire        rfd_f;
+
+   reg [19:0]  dividend_a;
+   reg [19:0]  dividend_b;
+   reg [19:0]  dividend_c;
+   reg [19:0]  dividend_d;
+   reg [19:0]  dividend_e;
+   reg [19:0]  dividend_f;
+
+   reg [9:0]   divisor_a;
+   reg [9:0]   divisor_b;
+   reg [9:0]   divisor_c;
+   reg [9:0]   divisor_d;
+   reg [9:0]   divisor_e;
+   reg [9:0]   divisor_f;
+
+   wire [19:0] quotient_a;
+   wire [19:0] quotient_b;
+   wire [19:0] quotient_c;
+   wire [19:0] quotient_d;
+   wire [19:0] quotient_e;
+   wire [19:0] quotient_f;
 
    // coordinates iterators in the untransformed images
    reg [9:0]  o_x;
    reg [8:0]  o_y;
+
+   reg [4:0]  counter;
+   reg 	      counting = 0;
+   
 
    parameter WAIT_FOR_CORNERS = 0;
    parameter CALC_INIT_DIST_MULT = 1;
@@ -99,6 +144,15 @@ module projective_transform(
    
    divc divider(.clk(clk), .rfd(rfd_c), .dividend(dividend_c),
 		.divisor(divisor_c), .quotient(quotient_c));
+
+   divd divider(.clk(clk), .rfd(rfd_d), .dividend(dividend_d),
+		.divisor(divisor_d), .quotient(quotient_e));
+
+   dive divider(.clk(clk), .rfd(rfd_e), .dividend(dividend_e),
+		.divisor(divisor_e), .quotient(quotient_d));
+   
+   divf divider(.clk(clk), .rfd(rfd_f), .dividend(dividend_f),
+		.divisor(divisor_f), .quotient(quotient_f));
 
    always @(posedge clk) begin
       case(state)
@@ -156,14 +210,27 @@ module projective_transform(
 	      divisor_e <= answer_c;
 	      divisor_f <= answer_c;
 
+	      counter <= 0;
+
 	      // update state
 	      state <= WAIT_FOR_DIVIDERS;
 	   end // if (sqrt_done_a && sqrt_done_b && sqrt_done_c)
 	end // case: WAIT_FOR_SQRT
 
 	WAIT_FOR_DIVIDERS: begin
-	   if (dividers are done???) begin
+	   counter <= counter + 1;
+
+	   // if divider is done (divider delay = M + 4)
+	   // M = dividend width = 20 in this case
+	   if (counter > 24;) begin
 	      wants_pixel <= 1;
+
+	      delta_a_x <= quotient_a;
+	      delta_a_y <= quotient_b;
+	      delta_b_x <= quotient_c;
+	      delta_b_y <= quotient_d;
+	      delta_c_x <= quotient_e;
+	      delta_c_y <= quotient_f;
 
 	      // update state
 	      state <= WAIT_FOR_PIXEL;
@@ -178,36 +245,97 @@ module projective_transform(
 	   // a new pixel has arrived, process accordingly
 	   if (pixel_flag) begin
 
+	      // output the new pixel coordinates
+	      pt_pixel_write <= pixel;
+	      pt_x <= i_c_x >> 10;
+	      pt_y <= i_c_y >> 10;
+
+	      // increment iterators
+	      i_c_x <= i_c_x + delta_c_x;
+	      i_c_y <= i_c_y + delta_c_y;
+	      o_x <= o_x + 1;
+
 	      // we are getting close to the end of this line. begin calculating the
 	      // next lines deltas and distances. 	   
 	      if (o_x == 600) begin
+		 // calculate distance squared
+		 temp1a <= (i_a_x + delta_a_x - (i_b_x + delta_b_x)) * (i_a_x + delta_a_x - (i_b_x + delta_b_x));
+		 temp1b <= (i_a_y + delta_a_y - (i_b_y + delta_b_y)) * (i_a_y + delta_a_y - (i_b_y + delta_b_y));
+	      end
+
+	      // after distance calculation has been completed
+	      if (o_x == 601) begin
+		 d_sqrt_a <= temp1a + temp1b;
+		 sqrt_start_a <= 1;
 	      end
 	   
 	      // the end of the line
 	      if (o_x == 640 && o_y < 480) begin
+		 // increment iterators
+		 o_y <= o_y + 1;
+		 i_a_x <= i_a_x + delta_a_x;
+		 i_a_y <= i_a_y + delta_a_y;
+		 i_b_x <= i_b_x + delta_b_x;
+		 i_b_y <= i_b_y + delta_b_y;
+
+		 // reset I_C to the new location of I_A
+		 i_c_x <= i_a_x + delta_a_x;
+		 i_c_y <= i_c_y + delta_a_y;
+
+		 // update the deltas
+		 delta_c_x <= delta_c_x_new;
+		 delta_c_y <= delta_c_y_new;
+
+		 // reset o_x
+		 o_x <= 0;
 	      end
 
 	      // the end of the frame
 	      if (o_x == 640 && o_y == 480) begin
-		 // reset everything
+		 // reset the iterator variables
+		 o_x <= 0;
+		 o_y <= 0;
+
+		 // the other iterators will be reset when new corners arrive
+		 
+		 // go back to waiting
 		 state <= WAIT_FOR_CORNERS;
 	      end
 	   end // if (pixel_flag)
 
+	   // if the square root operation is done, we can star the divisions
 	   if (sqrt_done_a) begin
-	      // start divider
-	   end
+	      // save distnace
+	      d_next_iterators <= answer_a;
 
-	   if (dividers a and b are done) begin
+	      // start dividers
+	      divisor_a <= answer_a;
+	      divisor_b <= answer_a;
+
+	      dividend_a <= ((i_b_x + delta_b_x) - (i_a_x + delta_a_x)) << 10;
+	      dividend_a <= ((i_b_y + delta_b_y) - (i_a_y + delta_a_y)) << 10;
+
+	      // reset counter;
+	      counter <= 0;
+	      // start counting;
+	      counting <= 1;
+	      
+	      
+	   end // if (sqrt_done_a)
+
+	   if (counting) counter <= counter + 1;
+
+	   // if the divider is done
+	   if (counter > 24) begin
 	      // save deltas
+	      delta_c_x_next <= quotient_a;
+	      delta_c_y_next <= quotient_b;
+	      counter <= 0; //reset counter
+	      counting <= 0; //stop counting
 	   end
 	   
 	end // case: WAIT_FOR_PIXEL
 	
-	   
-	CALC_NEXT_DIST: begin
-	end
-
       endcase // case (state)
    end // always @ (posedge clk)    
 endmodule // projective_transform
