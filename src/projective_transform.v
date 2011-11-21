2f23c32c9208c69bd9771d4822306df9fad5e7d6
Logan Williams <loganw@mit.edu>
2011-11-21 11:02:06 -0500

    Revised state machine of projective_transform

--- src/projective_transform.v
+++ src/projective_transform.v
@@ -1,10 +1,10 @@
 module projective_transform(input clk, frame_flag, [17:0] pixel, pixel_flag,
 			    [9:0] ax, [8:0] ay, [9:0] bx, [8:0] by, [9:0] cx, [8:0] cy,
-			    [9:0] dx, [8:0] dy, output reg [17:0] pixel_out, [9:0] pixel_x,
-			    [8:0] pixel_y, pixel_out_flag);
+			    [9:0] dx, [8:0] dy, corners_flag, output reg [17:0] pixel_out, [9:0] pixel_x,
+			    [8:0] pixel_y, pixel_out_flag), wants_pixel;
    
 
-   reg [?:0] state; // not sure how many states we have yet
+   reg [2:0] state = 0;
 
    // iterator coordinates
    reg [19:0] i_a_x;
@@ -20,142 +20,179 @@ module projective_transform(input clk, frame_flag, [17:0] pixel, pixel_flag,
    reg [9:0]  d_iterators;
 
    // temporary registers for calculating distances
-   reg [19:0] dcalc_temp1;
-   reg [19:0] dcalc_temp2;
-   reg [20:0] d_sqrt;
-   wire [10:0] answer;
-   wire        sqrt_done;
+   reg [19:0] temp1a;
+   reg [19:0] temp2a;
+   reg [19:0] temp1b;
+   reg [19:0] temp2b;
+   reg [19:0] temp1c;
+   reg [19:0] temp2c;
+
+   // wires/registers for sqrting
+   reg 	      sqrt_start_a;
+   reg 	      sqrt_start_b;
+   reg 	      sqrt_start_c;
+   
+   reg [20:0] d_sqrt_a;
+   reg [20:0] d_sqrt_b;
+   reg [20:0] d_sqrt_c;
+   
+   wire [10:0] answer_a;
+   wire [10:0] answer_b;
+   wire [10:0] answer_c;
+   
+   wire        sqrt_done_a;
+   wire        sqrt_done_c;
+   wire        sqrt_done_b;
+   
 
    // coordinates of the untransformed images
    reg [9:0]  o_x;
    reg [8:0]  o_y;
 
+   parameter WAIT_FOR_CORNERS = 0;
+   parameter CALC_INIT_DIST_MULT = 1;
+   parameter WAIT_FOR_SQRT = 2;
+   parameter WAIT_FOR_DIVIDERS = 3;
+   parameter WAIT_FOR_PIXEL = 4;
+   parameter CALC_NEXT_DIST = 5;
+   
+
    // this is the iterative square rooter used for distance calculations
    // with NBITS = 21, it will take 21 clock cycles to return a result.
-   distance_sqrter (NBITS = 21) sqrt(.clk(clk), .start(sqrt_start),
-				     .data(d_sqrt), .answer(answer),
-				     .done(sqrt_done));
+   sqrta (NBITS = 21) sqrt(.clk(clk), .start(sqrt_start_a),
+			   .data(d_sqrt_a), .answer(answer_a),
+			   .done(sqrt_done_a));
+
+   sqrtb (NBITS = 21) sqrt(.clk(clk), .start(sqrt_start_b),
+			   .data(d_sqrt_b), .answer(answer_b),
+			   .done(sqrt_done_b));
+
+   sqrtc (NBITS = 21) sqrt(.clk(clk), .start(sqrt_start_c),
+			   .data(d_sqrt_c), .answer(answer_c),
+			   .done(sqrt_done_c));
+
+   // three dividers, for parallelization. these are used to calculate
+   // iteration "deltas"
+   diva divider(.clk(clk), .rfd(rfd_a), .dividend(dividend_a),
+		.divisor(divisor_a), .quotient(quotient_a));
+
+   divb divider(.clk(clk), .rfd(rfd_b), .dividend(dividend_b),
+		.divisor(divisor_b), .quotient(quotient_b));
    
-   initial state = WAITING_FOR_FRAME_FLAG;
-   
+   divc divider(.clk(clk), .rfd(rfd_c), .dividend(dividend_c),
+		.divisor(divisor_c), .quotient(quotient_c));
+
    always @(posedge clk) begin
-      // this should be made part of the state machine
-      if (frame_flag) begin
-	 // initialize system parameters
-	 state <= CALCULATING_DISTANCE_AD_1;
-	 i_a_x <= ax << 10;
-	 i_a_y <= ay << 10;
-	 i_b_x <= bx << 10;
-	 i_b_y <= by << 10;
-
-	 d_ad <= 0;
-	 d_bc <= 0;
-	 d_iterators <= 0;
-
-	 o_x <= 0;
-	 o_y <= 0;
-      end
-
       case(state)
-	// once per frame
-	CALCULATING_DISTANCE_AD_1: begin
-	   dcalc_temp1 <= (ax - dx) * (ax - dx);
-	   dcalc_temp2 <= (ay - dy) * (ay - dy);
-	   state <= CALCULATING_DISTANCE_AD_2;
-	end
+	WAIT_FOR_CORNERS: begin
+	   if (corners_flag) begin
+	      // calculate distances squared
+	      temp1a <= (a_x - d_x) * (a_x - d_x);
+	      temp2a <= (a_y - d_y) * (a_y - d_y);
+	      temp1b <= (b_x - c_x) * (b_x - c_x);
+	      temp2b <= (b_y - c_y) * (b_y - c_y);
+	      temp1c <= (a_x - b_x) * (a_x - b_x);
+	      temp2c <= (a_y - b_y) * (a_y - b_y);
 
-	// once per frame
-	CALCULATING_DISTANCE_AD_Y: begin
-	   d_sqrt <= dcalc_temp1 + dcalc_temp2;
-	   sqrt_start <= 1;
-	   state <= CALCULATING_DISTANCE_AD_SQRT;
-	end
+	      // change state
+	      state <= CALC_INIT_DIST_MULT;
+	   end // if (corners_flag)
+	end // case: WAIT_FOR_CORNERS
 
-	// once per frame
-	CALCULATING_DISTANCE_AD_SQRT: begin
-	   sqrt_start <= 0;
+	CALC_INIT_DIST_MULT: begin
+	   // start square rooters
+	   d_sqrt_a <= temp1a + temp2a;
+	   d_sqrt_b <= temp1b + temp2b;
+	   d_sqrt_c <= temp1c + temp2c;
+	   sqrt_start_a <= 1;
+	   sqrt_start_b <= 1;
+	   sqrt_start_c <= 1;
 
-	   if (sqrt_done) begin
-	      d_ad <= answer;
+	   // go to the wait for sqrt state;
+	   state <= WAIT_FOR_SQRT;
+	end // case: CALC_INIT_DIST_MULT
 
-	      // will need two coregen divider modules
-	      delta_a_x = ((d_x - a_x) << 10) / answer;
-	      delta_a_y = ((d_y - a_y) << 10) / answer;
-	    	      
-	      state <= CALCULATING_DISTANCE_BC_1;
+	WAIT_FOR_SQRT: begin
+	   sqrt_start_a <= 0;
+	   sqrt_start_b <= 0;
+	   sqrt_start_c <= 0;
+
+	   if (sqrt_done_a && sqrt_done_b && sqrt_done_c) begin
+	      //save distances 
+	      d_ad <= answer_a;
+	      d_bc <= answer_b;
+	      d_iterators <= answer_c;
+
+	      //start dividers
+	      dividend_a <= (c_x - b_x) << 10;
+	      dividend_b <= (c_y - b_y) << 10;
+	      dividend_c <= (d_x - a_x) << 10;
+	      dividend_d <= (d_y - a_y) << 10;
+	      dividend_e <= (i_b_x - i_a_x) << 10;
+	      dividend_f <= (i_b_y - i_a_y) << 10;
+
+	      divisor_a <= answer_a;
+	      divisor_b <= answer_a;
+	      divisor_c <= answer_b;
+	      divisor_d <= answer_b;
+	      divisor_e <= answer_c;
+	      divisor_f <= answer_c;
+
+	      // update state
+	      state <= WAIT_FOR_DIVIDERS;
+	   end // if (sqrt_done_a && sqrt_done_b && sqrt_done_c)
+	end // case: WAIT_FOR_SQRT
+
+	WAIT_FOR_DIVIDERS: begin
+	   if (dividers are done???) begin
+	      wants_pixel <= 1;
+
+	      // update state
+	      state <= WAIT_FOR_PIXEL;
 	   end
 	end
 
-	// once per frame
-	CALCULATING_DISTANCE_BC_1: begin
-	   dcalc_temp1 <= (bx - cx) * (bx - cx);
-	   dcalc_temp2 <= (by - cy) * (by - cy);
-	   state <= CALCULATING_DISTANCE_BC_2;
-	end
+	// This is the state where the bulk of the module is accomplished.
+	// This waits for LPF to send a new pixel value to projective_transform,
+	// then echoes that value and new coords to the memory management module.
+	// then it increments the iterators accordingly.
+	WAIT_FOR_PIXEL: begin
+	   // a new pixel has arrived, process accordingly
+	   if (pixel_flag) begin
 
-	// once per frame
-	CALCULATING_DISTANCE_BC_2: begin
-	   dsqrt <= dcalc_temp1 + dcalc_temp2;
-	   sqrt_start <= 1;
-	   state <= CALCULATING_DISTANCE_BC_SQRT;
-	end
-
-	// once per frame
-	CALCULATING_DISTANCE_BC_SQRT: begin
-	   sqrt_start <= 0;
-
-	   if (sqrt_done) begin
-	      d_bc <= answer;
-
-	      // will need two coregen divider modules
-	      delta_b_x = ((c_x - b_x) << 10) / answer;
-	      delta_b_y = ((c_y - b_y) << 10) / answer;
-	      
-	      state <= CALCULATING_DISTANCE_ITERATORS_1;
-	   end
-	end
-
-	// once per LINE
-	// the next three state should be optimized to over lap with the end
-	// of each horizontal iteration, so that every line doesn't begin with
-	// a 20 clk cycle delay while the square root iterator is working
-	CALCULATING_DISTANCE_ITERATORS_1: begin
-	   dcalc_temp1 <= (i_a_x - i_b_x) * (i_a_x - i_b_x);
-	   dcalc_temp2 <= (i_a_y - i_b_y) * (i_a_y - i_b_y);
-	   state <= CALCULATING_DISTANCE_ITERATORS_2;
-	end
-
-	// once per LINE
-	CALCULATING_DISTANCE_ITERATORS_2: begin
-	   dsqrt <= dcalc_temp1 + dcalc_temp2;
-	   sqrt_start <= 1;
-	   state <= CALCULATING_DISTANCE_ITERATORS_SQRT;
-	end
-
-	// once per LINE
-	CALCULATE_DISTANCE_ITERATORS_SQRT: begin
-	   sqrt_start <= 0;
-
-	   if (sqrt_done) begin
-	      d_iterators <= answer;
-	      
-	      delta_c_x <= ((i_b_x - i_a_x) << 10) / answer;
-	      delta_c_y <= ((i_b_y - i_a_y) << 10) / answer;
-	      
-	      state <= WRITE_PIXEL;
-	   end
-	end
-
-
-	// ONCE PER PIXEL
-	WRITE PIXEL: begin
+	      // we are getting close to the end of this line. begin calculating the
+	      // next lines deltas and distances. 	   
+	      if (o_x == 600) begin
+	      end
 	   
+	      // the end of the line
+	      if (o_x == 640 && o_y < 480) begin
+	      end
+
+	      // the end of the frame
+	      if (o_x == 640 && o_y == 480) begin
+		 // reset everything
+		 state <= WAIT_FOR_CORNERS;
+	      end
+	   end // if (pixel_flag)
+
+	   if (sqrt_done_a) begin
+	      // start divider
+	   end
+
+	   if (dividers a and b are done) begin
+	      // save deltas
+	   end
+	   
+	end // case: WAIT_FOR_PIXEL
+	
+	   
+	CALC_NEXT_DIST: begin
 	end
 
-      endcase
-   end
-endmodule
-
+      endcase // case (state)
+   end // always @ (posedge clk)    
+endmodule // projective_transform
 
 // takes integer square root iteratively
 module sqrt #(parameter NBITS = 8,  // max 32
