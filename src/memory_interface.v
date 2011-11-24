6719781a207956c585c05617d0d8af309afda062
jecs <jecs@lambdalabs.com>
2011-11-23 20:07:22 -0500

    finished draft of memory_interface (untested)
    - finished logic for done flags
    - moved memory inputs and outputs to inputs and outputs list
    - finalized ordinal list
    - added queue logic for delayed reads

--- src/memory_interface.v
+++ src/memory_interface.v
@@ -6,15 +6,15 @@
 
 module memory_interface
 	(
-		// standard inputs
+		// STANDARD SIGNALS
 		input clock,
 		input reset,
-		// ntsc_capture
+		// NTSC_CAPTURE
 		input frame_flag,
 		input ntsc_flag,
 		input [`LOG_MEM:0] ntsc_pixel,
 		output reg done_ntsc,
-		// lpf
+		// LPF
 		input lpf_flag,
 		input lpf_wr,
 		input [`LOG_WIDTH:0] lpf_x,
@@ -22,28 +22,42 @@ module memory_interface
 		input [`LOG_MEM:0] lpf_pixel_write,
 		output reg done_lpf,
 		output reg [`LOG_MEM:0] lpf_pixel_read,
-		// projective_transform inputs
+		// PROJECTIVE_TRANSFORM
 		input pt_flag,
 		input pt_wr,
 		input [`LOG_WIDTH:0] pt_x,
 		input [`LOG_HEIGHT:0] pt_y,
 		input [`LOG_TRUNC:0] pt_pixel_write,
 		output reg done_pt,
-		// vga_write inputs
+		// VGA_WRITE
 		input vga_flag,
 		output done_vga,
-		output reg [`LOG_FULL:0] vga_pixel
+		output reg [`LOG_FULL:0] vga_pixel,
+		// MEMORY
+		// MEM ADDRESSES
+		output reg [`LOG_ADDR:0] mem0_addr,
+		output reg [`LOG_ADDR:0] mem1_addr,	
+		// MEM READ	
+		input [`LOG_MEM:0] mem0_read;
+		input [`LOG_MEM:0] mem1_read;
+		// MEM WRITE
+		output reg [`LOG_MEM:0] mem0_write;
+		output reg [`LOG_MEM:0] mem1_write;
+		// WR FLAGS
+		output reg mem0_wr;
+		output reg mem1_wr;
 	);
 
 	/******** PARAMETERS ********/
 	// READ QUEUE LENGTH
 	parameter QUEUE_LENGTH = 3;
 	// MODULE ORDINALS
-	parameter NTSC = 2'd0;
-	parameter LPF  = 2'd1;
-	parameter PT   = 2'd2;
-	parameter VGA  = 2'd3;
-	parameter LOG_ORD = 2;
+	parameter NTSC = 4'b1000;
+	parameter VGA  = 4'b0100;
+	parameter LPF  = 4'b0010;
+	parameter PT   = 4'd0001;
+	parameter NONE = 4'd0000;
+	parameter LOG_ORD = 4;
 	/****************************/
 
 	// BLOCK OF SRAM IMAGE IS IN
@@ -71,20 +85,18 @@ module memory_interface
 	reg [3:0] next_blocks;
 	reg [7:0] next_locs;
 
-	// MEMORY
-	// MEM ADDRESSES
-	reg [`LOG_ADDR:0] mem0_addr;
-	reg [`LOG_ADDR:0] mem1_addr;	
-	// MEM READ	
-	wire [`LOG_MEM:0] mem0_read;
-	wire [`LOG_MEM:0] mem1_read;
-	// MEM WRITE
-	reg [`LOG_MEM:0] mem0_write;
-	reg [`LOG_MEM:0] mem1_write;
+	// PARTIAL DONE FLAGS
+	reg [3:0] mem0_done;
+	reg [3:0] mem1_done;
 	
 	// READ QUEUES
 	reg [QUEUE_LENGTH*LOG_ORD-1:0] mem0_read_queue;
 	reg [QUEUE_LENGTH*LOG_ORD-1:0] mem1_read_queue;
+	reg [QUEUE_LENGTH*LOG_ORD-1:0] next_mem0_read_queue;
+	reg [QUEUE_LENGTH*LOG_ORD-1:0] next_mem1_read_queue;
+	// ELEMENTS AT END OF QUEUES
+	reg [LOG_ORD-1:0] mem0_next_read;
+	reg [LOG_ORD-1:0] mem1_next_read;
 
 	always @(*) begin
 		// shifting
@@ -107,33 +119,121 @@ module memory_interface
 		lpf_addr = (`IMAGE_WIDTH * lpf_y) + lpf_x + (proc_mem_loc * `IMAGE_LENGTH);
 		pt_addr = (`IMAGE_WIDTH * pt_y) + pt_x + (next_mem_loc * `IMAGE_LENGTH);
 
-		// set address
-		if (!capt_mem_block && ntsc_flag) mem0_addr = ntsc_addr;
-		else if (!disp_mem_block && vga_flag) mem0_addr = vga_addr;
-		else if (!proc_mem_block && lpf_flag) mem0_addr = lpf_addr;
-		else if (!next_mem_block && pt_flag) mem0_addr = pt_addr;
+		// set address & write & done flags
+		// assign write value to mem0 & mem1 based on who's writing
+		if (!capt_mem_block && ntsc_flag) begin
+			mem0_addr = ntsc_addr;
+			mem0_write = ntsc_pixel;
+			mem0_wr = 1;
+			mem0_done = NTSC;
+		end
+		else if (!disp_mem_block && vga_flag) begin
+			mem0_addr = vga_addr;
+			mem0_wr = 0;
+			mem0_done = VGA;
+		end
+		else if (!proc_mem_block && lpf_flag) begin
+			mem0_addr = lpf_addr;
+			mem0_write = lpf_pixel_write;
+			mem0_wr = lpf_wr;
+			mem0_done = LPF;
+		end
+		else if (!next_mem_block && pt_flag) begin
+			mem0_addr = pt_addr;
+			mem0_write = pt_pixel_write;
+			mem0_wr = pt_wr;
+			mem0_done = PT;
+		end
+		else begin // nothing's happening
+			mem0_addr = 0;
+			mem0_write = 0;
+			mem0_wr = 0;
+			mem0_done = NONE;
+		end
+
+		if (capt_mem_block && ntsc_flag) begin
+			mem1_addr = ntsc_addr;
+			mem1_write = ntsc_pixel;
+			mem1_wr = 1;
+			mem1_done = NTSC;
+		end
+		else if (disp_mem_block && vga_flag) begin
+			mem1_addr = vga_addr;
+			mem1_wr = 0;
+			mem1_done = VGA;
+		end
+		else if (proc_mem_block && lpf_flag) begin
+			mem1_addr = lpf_addr;
+			mem1_write = lpf_pixel;
+			mem1_wr = lpf_wr;
+			mem1_done = LPF;
+		end
+		else if (next_mem_block && pt_flag) begin
+			mem1_addr = pt_addr;
+			mem1_write = pt_pixel;
+			mem1_wr = pt_wr;
+			mem1_done = PT;
+		end
+		else begin // nothing's happening
+			mem1_addr = 0;
+			mem1_write = 0;
+			mem1_wr = 0;
+			mem1_done = NONE;
+		end
+
+		// set done flags
+		done_ntsc = mem0_done[3] || mem1_done[3];
+		done_vga  = mem0_done[2] || mem1_done[2];
+		done_lpf  = mem0_done[1] || mem1_done[1];
+		done_pt   = mem0_done[0] || mem1_done[0];
 
 		// assign read value to corresponding member of queue
+		mem0_next_read = mem0_read_queue[QUEUE_LENGTH*LOG_ORD-1:(QUEUE_LENGTH-1)*LOG_ORD];
+		mem1_next_read = mem1_read_queue[QUEUE_LENGTH*LOG_ORD-1:(QUEUE_LENGTH-1)*LOG_ORD];
 		
-		// assign write value to mem0 & mem1 based on who's writing
+		// LPF's turn
+		if (mem0_next_read == LPF) lpf_pixel_read = mem0_read;
+		else if (mem1_next_read == LPF) lpf_pixel_read = mem1_read;
+		else lpf_pixel_read = 0;
 
-		// update queue
+		// VGA's turn
+		if (mem0_next_read == VGA) vga_pixel = mem0_read;
+		else if (mem1_next_read == VGA) vga_pixel = mem1_read;
+		else vga_pixel = 0;
+
+		// shifting of queue on next cycle
+		next_mem0_read_queue[QUEUE_LENGTH*LOG_ORD-1:LOG_ORD] = mem0_read_queue[(QUEUE_LENGTH-1)*LOG_ORD-1:0];
+		next_mem1_read_queue[QUEUE_LENGTH*LOG_ORD-1:LOG_ORD] = mem1_read_queue[(QUEUE_LENGTH-1)*LOG_ORD-1:0];
+
+		// add new queue members, if any
+		if (mem0_done == VGA) next_mem0_read_queue[LOG_ORD-1:0] = VGA;
+		else if (mem0_done == LPF && !lpf_wr) next_mem0_read_queue[LOG_ORD-1:0] = LPF;
+		else next_mem0_read_queue[LOG_ORD-1:0] = NONE;
+
+		if (mem1_done == VGA) next_mem1_read_queue[LOG_ORD-1:0] = VGA;
+		else if (mem1_done == LPF && !lpf_wr) next_mem1_read_queue[LOG_ORD-1:0] = LPF;
+		else next_mem1_read_queue[LOG_ORD-1:0] = NONE;
 
 		// this should be it
 	end
 
 	always @(posedge clock) begin
+		// update blocks and locations of images in RAM
 		{capt_mem_block, proc_mem_block, nexd_mem_block, disp_mem_block} <= next_blocks;
 		{capt_mem_loc, proc_mem_loc, nexd_mem_loc, disp_mem_loc} <= next_locs;
 
 		// set addresses of NTSC and VGA / update if pixels have been read or written
-		ntsc_addr <= (reset) ? 0 : (ntsc_addr + done_ntsc);
-		vga_addr <= (reset) ? 0 : (vga_addr + done_vga);
+		// set to 0 at the start of each frame or when the FPGA is reset
+		ntsc_addr <= (reset || frame_flag) ? 0 : (ntsc_addr + done_ntsc);
+		vga_addr <= (reset || frame_glag) ? 0 : (vga_addr + done_vga);
+
+		// update read queues
+		mem0_read_queue <= next_mem0_read_queue;
+		mem1_read_queue <= next_mem1_read_queue;
 	end
 endmodule
 
 // TODO:
-// - add logic for all of the done flags
 // - interface with memory
 
 /* FOR REFERENCE
