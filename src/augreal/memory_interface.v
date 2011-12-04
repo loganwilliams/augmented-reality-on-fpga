0f3fcc3f70f18c77965e92af6a2cf4caa1698a7a
jecs <jecs@lambdalabs.com>
2011-12-04 18:42:49 -0500

    memory_interface is now sequential (ie, 3 cycle delay)

--- src/augreal/memory_interface.v
+++ src/augreal/memory_interface.v
@@ -8,57 +8,55 @@
 module memory_interface
 	(
 		// STANDARD SIGNALS
-		input 			   clock,
-		input 			   reset,
+		input clock,
+		input reset,
 		// NTSC_CAPTURE
-		input 			   frame_flag,
-		input 			   ntsc_flag,
-		input [`LOG_MEM-1:0] 	   ntsc_pixel,
-		output reg 		   done_ntsc,
+		input frame_flag,
+		input ntsc_flag,
+		input [`LOG_MEM-1:0] ntsc_pixel,
+		output reg done_ntsc,
 		input [`LOG_WIDTH-1:0] ntsc_x,
 		input [`LOG_HEIGHT-1:0] ntsc_y,
 		// LPF
-		input 			   lpf_flag,
-		input 			   lpf_wr,
-		input [`LOG_WIDTH-1:0] 	   lpf_x,
-		input [`LOG_HEIGHT-1:0]    lpf_y,
-		input [`LOG_MEM-1:0] 	   lpf_pixel_write,
-		output reg 		   done_lpf,
-		output reg [`LOG_MEM-1:0]  lpf_pixel_read,
+		input lpf_flag,
+		input lpf_wr,
+		input [`LOG_WIDTH-1:0] lpf_x,
+		input [`LOG_HEIGHT-1:0] lpf_y,
+		input [`LOG_MEM-1:0] lpf_pixel_write,
+		output reg done_lpf,
+		output reg [`LOG_MEM-1:0] lpf_pixel_read,
 		// PROJECTIVE_TRANSFORM
-		input 			   pt_flag,
-		input [`LOG_WIDTH-1:0] 	   pt_x,
-		input [`LOG_HEIGHT-1:0]    pt_y,
-		input [`LOG_TRUNC-1:0] 	   pt_pixel,
-		output 			   done_pt,
+		input pt_flag,
+		input [`LOG_WIDTH-1:0] pt_x,
+		input [`LOG_HEIGHT-1:0] pt_y,
+		input [`LOG_TRUNC-1:0] pt_pixel,
+		output done_pt,
 		// VGA_WRITE
-		input 			   vga_flag,
-		output reg 		   done_vga,
-		output reg [`LOG_MEM-1:0]  vga_pixel,
-		input 	[`LOG_VCOUNT-1:0]		   vcount,
-		input 	[`LOG_HCOUNT-1:0]		   hcount,
-	 	input 			   vsync,
+		input vga_flag,
+		output reg done_vga,
+		output reg [`LOG_MEM-1:0] vga_pixel,
+		input [`LOG_VCOUNT-1:0] vcount,
+		input [`LOG_HCOUNT-1:0] hcount,
+	 	input vsync,
 		// MEMORY
 		// MEM ADDRESSES
 		output reg [`LOG_ADDR-1:0] mem0_addr,
 		output reg [`LOG_ADDR-1:0] mem1_addr, 
 		// MEM READ	
-		input [`LOG_MEM-1:0] 	   mem0_read,
-		input [`LOG_MEM-1:0] 	   mem1_read,
+		input [`LOG_MEM-1:0] mem0_read,
+		input [`LOG_MEM-1:0] mem1_read,
 		// MEM WRITE
-		output reg [`LOG_MEM-1:0]  mem0_write,
-		output reg [`LOG_MEM-1:0]  mem1_write,
+		output reg [`LOG_MEM-1:0] mem0_write,
+		output reg [`LOG_MEM-1:0] mem1_write,
 		// WR FLAGS
-		output reg 		   mem0_wr,
-		output reg 		   mem1_wr,
+		output reg mem0_wr,
+		output reg mem1_wr,
 		// TESTING
-		output [3:0] 		   debug_blocks,
-		output [7:0] 		   debug_locs
+		output [3:0] debug_blocks,
+		output [7:0] debug_locs
 	);
 
 	/******** PARAMETERS ********/
-	// READ QUEUE LENGTH
-	parameter QUEUE_LENGTH = 2;
 	// MODULE ORDINALS
 	parameter NTSC = 4'b1000;
 	parameter VGA  = 4'b0100;
@@ -101,11 +99,9 @@ module memory_interface
 	reg [3:0] mem1_done;
 	
 	// READ QUEUES
-	reg [QUEUE_LENGTH*LOG_ORD-1:0] mem0_read_queue;
-	reg [QUEUE_LENGTH*LOG_ORD-1:0] mem1_read_queue;
-	reg [QUEUE_LENGTH*LOG_ORD-1:0] next_mem0_read_queue;
-	reg [QUEUE_LENGTH*LOG_ORD-1:0] next_mem1_read_queue;
-	// ELEMENTS AT END OF QUEUES
+	reg [LOG_ORD-1:0] mem0_read_queue;
+	reg [LOG_ORD-1:0] mem1_read_queue;
+	// ELEMENT TO BE OUTPUT FROM MEM AT NEXT CYCLE
 	reg [LOG_ORD-1:0] mem0_next_read;
 	reg [LOG_ORD-1:0] mem1_next_read;
 
@@ -119,78 +115,93 @@ module memory_interface
 	assign debug_blocks = {capt_mem_block,proc_mem_block,nexd_mem_block,disp_mem_block};
 	assign debug_locs = {capt_mem_loc, proc_mem_loc, nexd_mem_loc, disp_mem_loc};
 
-	always @(*) begin
+	always @(posedge clock) begin
 		// set address & write & done flags
 		// assign write value to mem0 & mem1 based on who's writing
 		if (!capt_mem_block && ntsc_flag) begin
-			mem0_addr = ntsc_addr;
-			mem0_write = ntsc_pixel;
-			mem0_wr = 1;
-			mem0_done = NTSC;
+			mem0_addr 	<= ntsc_addr;
+			mem0_write 	<= ntsc_pixel;
+			mem0_wr 	<= 1;
+			mem0_done 	<= NTSC;
 		end
 		else if (!disp_mem_block && vga_flag) begin
-			mem0_addr = vga_addr;
-			mem0_wr = 0;
-			mem0_done = VGA;
+			mem0_addr 	<= vga_addr;
+			mem0_write 	<= mem0_write;
+			mem0_wr 	<= 0;
+			mem0_done 	<= VGA;
 		end
 		else if (!proc_mem_block && lpf_flag) begin
-			mem0_addr = lpf_addr;
-			mem0_write = lpf_pixel_write;
-			mem0_wr = lpf_wr;
-			mem0_done = LPF;
+			mem0_addr 	<= lpf_addr;
+			mem0_write 	<= lpf_pixel_write;
+			mem0_wr 	<= lpf_wr;
+			mem0_done 	<= LPF;
 		end
 		else if (!nexd_mem_block && ptf_flag) begin
-			mem0_addr = ptf_addr;
-			mem0_write = ptf_pixel_write;
-			mem0_wr = ptf_wr;
-			mem0_done = PTF;
+			mem0_addr 	<= ptf_addr;
+			mem0_write 	<= ptf_pixel_write;
+			mem0_wr 	<= ptf_wr;
+			mem0_done 	<= PTF;
 		end 
 		else begin // nothing's happening
-			mem0_addr = 0;
-			mem0_write = 0;
-			mem0_wr = 0;
-			mem0_done = NONE;
+			mem0_addr 	<= 0;
+			mem0_write 	<= 0;
+			mem0_wr 	<= 0;
+			mem0_done 	<= NONE;
 		end
 
 		if (capt_mem_block && ntsc_flag) begin
-			mem1_addr = ntsc_addr;
-			mem1_write = ntsc_pixel;
-			mem1_wr = 1;
-			mem1_done = NTSC;
+			mem1_addr 	<= ntsc_addr;
+			mem1_write 	<= ntsc_pixel;
+			mem1_wr 	<= 1;
+			mem1_done 	<= NTSC;
 		end
 		else if (disp_mem_block && vga_flag) begin
-			mem1_addr = vga_addr;
-			mem1_wr = 0;
-			mem1_done = VGA;
+			mem1_addr 	<= vga_addr;
+			mem1_write	<= mem1_write;
+			mem1_wr 	<= 0;
+			mem1_done 	<= VGA;
 		end
 		else if (proc_mem_block && lpf_flag) begin
-			mem1_addr = lpf_addr;
-			mem1_write = lpf_pixel_write;
-			mem1_wr = lpf_wr;
-			mem1_done = LPF;
+			mem1_addr 	<= lpf_addr;
+			mem1_write 	<= lpf_pixel_write;
+			mem1_wr 	<= lpf_wr;
+			mem1_done 	<= LPF;
 		end
 		else if (nexd_mem_block && ptf_flag) begin
-			mem1_addr = ptf_addr;
-			mem1_write = ptf_pixel_write;
-			mem1_wr = ptf_wr;
-			mem1_done = PTF;
+			mem1_addr 	<= ptf_addr;
+			mem1_write 	<= ptf_pixel_write;
+			mem1_wr 	<= ptf_wr;
+			mem1_done 	<= PTF;
 		end
 		else begin // nothing's happening
-			mem1_addr = 0;
-			mem1_write = 0;
-			mem1_wr = 0;
-			mem1_done = NONE;
+			mem1_addr 	<= 0;
+			mem1_write 	<= 0;
+			mem1_wr 	<= 0;
+			mem1_done 	<= NONE;
 		end
 
+		// add new queue members, if any
+		if (mem0_done == VGA) mem0_read_queue[LOG_ORD-1:0] <= VGA;
+		else if (mem0_done == LPF && !lpf_wr) mem0_read_queue[LOG_ORD-1:0] <= LPF;
+		else if (mem0_done == PTF && !ptf_wr) mem0_read_queue[LOG_ORD-1:0] <= PTF;
+		else mem0_read_queue[LOG_ORD-1:0] <= NONE;
+
+		if (mem1_done == VGA) mem1_read_queue[LOG_ORD-1:0] <= VGA;
+		else if (mem1_done == LPF && !lpf_wr) mem1_read_queue[LOG_ORD-1:0] <= LPF;
+		else if (mem1_done == PTF && !ptf_wr) mem1_read_queue[LOG_ORD-1:0] <= PTF;
+		else mem1_read_queue[LOG_ORD-1:0] <= NONE;
+
+		// assign read value to corresponding member of queue
+		mem0_next_read <= mem0_read_queue[LOG_ORD-1:0];
+		mem1_next_read <= mem1_read_queue[LOG_ORD-1:0];
+	end
+
+	always @(*) begin
 		// set done flags
 		done_ntsc = (mem0_done == NTSC) || (mem1_done == NTSC);
 		done_vga  = (mem0_done == VGA)  || (mem1_done == VGA);
 		done_lpf  = (mem0_done == LPF)  || (mem1_done == LPF);
 		done_ptf  = (mem0_done == PTF)  || (mem1_done == PTF);
-
-		// assign read value to corresponding member of queue
-		mem0_next_read = mem0_read_queue[QUEUE_LENGTH*LOG_ORD-1:(QUEUE_LENGTH-1)*LOG_ORD];
-		mem1_next_read = mem1_read_queue[QUEUE_LENGTH*LOG_ORD-1:(QUEUE_LENGTH-1)*LOG_ORD];
 		
 		// LPF's turn in the queue
 		if (mem0_next_read == LPF) lpf_pixel_read = mem0_read;
@@ -206,21 +217,6 @@ module memory_interface
 		if (mem0_next_read == VGA) vga_pixel = mem0_read;
 		else if (mem1_next_read == VGA) vga_pixel = mem1_read;
 		else vga_pixel = prev_vga_pixel;
-
-		// shifting of queue on next cycle
-		next_mem0_read_queue[QUEUE_LENGTH*LOG_ORD-1:LOG_ORD] = mem0_read_queue[(QUEUE_LENGTH-1)*LOG_ORD-1:0];
-		next_mem1_read_queue[QUEUE_LENGTH*LOG_ORD-1:LOG_ORD] = mem1_read_queue[(QUEUE_LENGTH-1)*LOG_ORD-1:0];
-
-		// add new queue members, if any
-		if (mem0_done == VGA) next_mem0_read_queue[LOG_ORD-1:0] = VGA;
-		else if (mem0_done == LPF && !lpf_wr) next_mem0_read_queue[LOG_ORD-1:0] = LPF;
-		else if (mem0_done == PTF && !ptf_wr) next_mem0_read_queue[LOG_ORD-1:0] = PTF;
-		else next_mem0_read_queue[LOG_ORD-1:0] = NONE;
-
-		if (mem1_done == VGA) next_mem1_read_queue[LOG_ORD-1:0] = VGA;
-		else if (mem1_done == LPF && !lpf_wr) next_mem1_read_queue[LOG_ORD-1:0] = LPF;
-		else if (mem1_done == PTF && !ptf_wr) next_mem1_read_queue[LOG_ORD-1:0] = PTF;
-		else next_mem1_read_queue[LOG_ORD-1:0] = NONE;
 		// this should be it
 	end
 
@@ -249,36 +245,28 @@ module memory_interface
 			nexd_mem_block 	<= 1'b1;
 			nexd_mem_loc 	<= 2'b00;
 			disp_mem_block 	<= 1'b1;
-			disp_mem_loc <= 2'b01;
+			disp_mem_loc 	<= 2'b01;
 		end
 		else if (frame_flag) begin
-			capt_mem_block 		<= proc_mem_block;
-			capt_mem_loc 		<= proc_mem_loc;
-			proc_mem_block 		<= disp_mem_block;
-			proc_mem_loc 		<= disp_mem_loc;
-			nexd_mem_block 		<= capt_mem_block;
-			nexd_mem_loc 		<= capt_mem_loc;
-			disp_mem_block 		<= nexd_mem_block;
-			disp_mem_loc 		<= nexd_mem_loc;
+			capt_mem_block	<= proc_mem_block;
+			capt_mem_loc 	<= proc_mem_loc;
+			proc_mem_block 	<= disp_mem_block;
+			proc_mem_loc 	<= disp_mem_loc;
+			nexd_mem_block	<= capt_mem_block;
+			nexd_mem_loc 	<= capt_mem_loc;
+			disp_mem_block	<= nexd_mem_block;
+			disp_mem_loc 	<= nexd_mem_loc;
 		end
 		else begin
-			capt_mem_block 		<= capt_mem_block;
-			capt_mem_loc		<= capt_mem_loc;
-			proc_mem_block		<= proc_mem_block;
-			proc_mem_loc		<= proc_mem_loc;
-			nexd_mem_block		<= nexd_mem_block;
-			nexd_mem_loc		<= nexd_mem_loc;
-			disp_mem_block		<= disp_mem_block;
-			disp_mem_loc		<= disp_mem_loc;
+			capt_mem_block 	<= capt_mem_block;
+			capt_mem_loc	<= capt_mem_loc;
+			proc_mem_block	<= proc_mem_block;
+			proc_mem_loc	<= proc_mem_loc;
+			nexd_mem_block	<= nexd_mem_block;
+			nexd_mem_loc	<= nexd_mem_loc;
+			disp_mem_block	<= disp_mem_block;
+			disp_mem_loc	<= disp_mem_loc;
 		end
-		
-		// update ntsc and vga addresses
-		//if (reset || frame_flag) ntsc_addr <= (proc_mem_loc*`IMAGE_LENGTH);
-		//else ntsc_addr <= (done_ntsc) ? (ntsc_addr+1) : ntsc_addr;	
-		
-		// update read queues
-		mem0_read_queue <= next_mem0_read_queue;
-		mem1_read_queue <= next_mem1_read_queue;
 
 		// retain previous output pixel values
 		prev_vga_pixel <= vga_pixel;
